53fc34165f84278579b5af1298798c724198f5a2
VFM | SB <vfm_sb@proton.me>
2023-12-24 21:27:37 +0200

    Initialization of Catalog Class -- Data Loaders
    modified:   coffee_machine/catalog.py
     - New: load_catalog_data()
     - New: load_drinks_data()
     - New: load_groups_data()
    new file:   coffee_machine/drink.py

--- coffee_machine/catalog.py
+++ coffee_machine/catalog.py
@@ -1,4 +1,8 @@
+# Built-in Modules
+import json
+
 # Local Modules
+from drink import Drink
 from catalog_builder import CatalogBuilderCLI
 
 # Utilities Imports
@@ -17,6 +21,9 @@ class Catalog:
         self.filename = filename
         self.filepath = self.get_catalog_filepath()
         self.name = self.get_catalog_name()
+        self.catalog_data = self.load_catalog_data()
+        self.drinks_data = self.load_drinks_data()
+        self.groups_data = self.load_groups_data()
 
     @property
     def filename(self) -> str:
@@ -35,4 +42,28 @@ class Catalog:
 
     def get_catalog_filepath(self) -> str:
         return get_file_path(filename=self.filename, path=Catalog.path)
+
+    # Data Loaders
+    def load_catalog_data(self) -> dict:
+        try:
+            with open(self.filepath, "r", encoding="UTF-8") as catalog_file:
+                return json.load(catalog_file)
+        except FileNotFoundError:
+            return {"drinks": {}, "groups": {}}
+
+    def load_drinks_data(self) -> dict:
+        drinks_data = {}
+        for menu_code, drink_data in self.catalog_data["drinks"].items():
+            menu_code = float(menu_code)
+            if not self.is_group(menu_code):
+                menu_code = int(menu_code)
+            drinks_data[menu_code] = Drink.construct(drink_data)
+        return drinks_data
+
+    def load_groups_data(self) -> dict:
+        groups_data = {}
+        for group_code, group_data in self.catalog_data["groups"].items():
+            group_code = int(group_code)
+            groups_data[group_code] = group_data
+        return groups_data
 
