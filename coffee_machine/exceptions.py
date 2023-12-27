434505330a975eaf05f9493653c6338515b3bf26
VFM | SB <vfm_sb@proton.me>
2023-12-27 22:02:24 +0200

    InvalidInputTypeError Exception
    > Deals with Invalid input_type of InputHandler
    
    modified:   coffee_machine/exceptions.py

--- coffee_machine/exceptions.py
+++ coffee_machine/exceptions.py
@@ -9,3 +9,11 @@ class InvalidFilenameError(Exception):
         if filename:
             self.message += f" >> {filename}"
         super().__init__(self.message)
+
+
+class InvalidInputTypeError(Exception):
+    def __init__(self, input_type: str | None) -> None:
+        self.message = "Invalid Input Type"
+        if input_type:
+            self.message += f' >> "{input_type}"'
+        super().__init__(self.message)
