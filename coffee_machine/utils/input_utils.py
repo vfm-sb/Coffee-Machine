a5c538e003e0544bfcd55c97eaa97c9c167ef1f1
VFM | SB <vfm_sb@proton.me>
2023-12-30 14:57:19 +0200

    Utils >> Input Utils -- extract_numeric_value()
    - Extracts Numeric Value from a String
    
    modified:   coffee_machine/utils/input_utils.py

--- coffee_machine/utils/input_utils.py
+++ coffee_machine/utils/input_utils.py
@@ -12,6 +12,14 @@ def get_numeric_value(input_string: str) -> int | float:
             raise InvalidNumericInputError(input_string)
 
 
+def extract_numeric_value(input_string: str) -> int | float:
+    value_string = str()
+    for char in input_string:
+        if char.isdigit() or char == ".":
+            value_string += char
+    return get_numeric_value(value_string)
+
+
 def split_by_comma(input_string: str) -> list:
     return [part.strip() for part in input_string.split(",")]
 
