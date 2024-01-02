9e4d19e8eaf2752fb48aa2a967ec895f8be157e6
VFM | SB <vfm_sb@proton.me>
2024-01-02 21:28:15 +0200

    CurrencyLoader >> New: load_currencies()
    - Included path & active_currencies
    
    modified:   coffee_machine/electrum/currency_loader.py

--- coffee_machine/electrum/currency_loader.py
+++ coffee_machine/electrum/currency_loader.py
@@ -1,3 +1,6 @@
+# Built-in Modules
+import json
+
 # Local Modules
 from coffee_machine.electrum import Currency
 
@@ -6,6 +9,8 @@ from coffee_machine.exceptions import InvalidCurrencyCodeError, CurrencyNotFound
 
 
 class CurrencyLoader:
+    path = "data/currencies"
+    active_currencies = "_CURRENCIES.json"
 
     def __init__(self, code: str | int) -> None:
         self.currencies = self.load_currencies()
@@ -15,4 +20,9 @@ class CurrencyLoader:
             raise CurrencyNotFoundError
         self.currency_id = self.get_currency_id(code)
         self.currency_data = self.load_currency_data()
+
+    def load_currencies(self) -> dict:
+        filepath = get_file_path(CurrencyLoader.active_currencies, CurrencyLoader.path)
+        with open(filepath, "r", encoding="UTF-8") as active_currencies_file:
+            return json.load(active_currencies_file)
 
