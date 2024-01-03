03605aa431733bdc2e8970323844fe9af937d636
VFM | SB <vfm_sb@proton.me>
2024-01-03 23:20:46 +0200

    Drink >> __init__()
    modified:   coffee_machine/drink.py

--- coffee_machine/drink.py
+++ coffee_machine/drink.py
@@ -1,4 +1,16 @@
 
 
 class Drink:
+
+    def __init__(
+        self, name: str, ingredients: dict, sweeteners: list, extras: dict
+    ) -> None:
+        self.name = name
+        self.ingredients = ingredients
+        self.sweeteners = sweeteners
+        self.extras = extras
+
+
+# Testing
+if __name__ == "__main__":
     pass
