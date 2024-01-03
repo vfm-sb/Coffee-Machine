99471d8267781a7c4dcdaffa8866680e6ce69b7d
VFM | SB <vfm_sb@proton.me>
2024-01-03 19:51:21 +0200

    Currency >> New: __init__() -- Initial Instance Attributes
    new file:   coffee_machine/electrum/currency.py

---
+++ coffee_machine/electrum/currency.py
@@ -0,0 +1,29 @@
+# Local Modules
+from coffee_machine.electrum import CurrencyLoader
+
+
+class Currency:
+
+    def __init__(self, code: str | int) -> None:
+        currency_data = CurrencyLoader(code).data
+        self.alphabetic_code = currency_data["iso-alphabetic"]
+        self.numeric_code = currency_data["iso-numeric"]
+        self.name = currency_data["name"]
+        self.base = currency_data["base"]
+        self.denominator = currency_data["denominator"]
+        self.precision = currency_data["precision"]
+        self.unit_name = currency_data["unit"]["name"]
+        self.unit_plural = currency_data["unit"]["plural"]
+        self.unit_symbol = currency_data["unit"]["symbol"]
+        self.unit_abbr = currency_data["unit"]["abbreviation"]
+        self.unit_symbol_format = currency_data["unit"]["formats"]["symbol"]
+        self.unit_abbr_format = currency_data["unit"]["formats"]["abbreviation"]
+        self.subunit_name = currency_data["subunit"]["name"]
+        self.subunit_plural = currency_data["subunit"]["plural"]
+        self.subunit_symbol = currency_data["subunit"]["symbol"]
+        self.subunit_abbr = currency_data["subunit"]["abbreviation"]
+        self.subunit_symbol_format = currency_data["subunit"]["formats"]["symbol"]
+        self.subunit_abbr_format = currency_data["subunit"]["formats"]["abbreviation"]
+        self.banknotes = currency_data["banknotes"]
+        self.coins = currency_data["coins"]
+        self.users = currency_data["users"]
