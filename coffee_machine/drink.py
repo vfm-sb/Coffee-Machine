6ec4d24a54e0034c99d6f3c6a210701b5c29e809
VFM | SB <vfm_sb@proton.me>
2024-01-03 23:21:52 +0200

    Drink >> get_drink_data() Method & construct() Class Method
    - get_drink_data() Returns Data Version of Drink Instance
    - @classmethod:construct() Returns Drink Instance of Given Data
    
    modified:   coffee_machine/drink.py

--- coffee_machine/drink.py
+++ coffee_machine/drink.py
@@ -10,6 +10,23 @@ class Drink:
         self.sweeteners = sweeteners
         self.extras = extras
 
+    def get_drink_data(self) -> dict:
+        return {
+            "name": self.name,
+            "ingredients": self.ingredients,
+            "sweeteners": self.sweeteners,
+            "extras": self.extras
+        }
+
+    @classmethod
+    def construct(cls, drink_data: dict) -> 'Drink':
+        return cls(
+            name = drink_data["name"],
+            ingredients = drink_data["ingredients"],
+            sweeteners = drink_data["sweeteners"],
+            extras = drink_data["extras"]
+        )
+
 
 # Testing
 if __name__ == "__main__":
