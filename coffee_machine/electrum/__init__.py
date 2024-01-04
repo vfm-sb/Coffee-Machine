78a9b17e97664971ed79a6249e1db5b8aec1fbb2
VFM | SB <vfm_sb@proton.me>
2024-01-04 22:52:34 +0200

    electrum >> __init__.py
    - Established Money Package
    
    new file:   coffee_machine/electrum/__init__.py

---
+++ coffee_machine/electrum/__init__.py
@@ -0,0 +1,7 @@
+from .money import Money
+from .coin import Coin
+
+from .currency import Currency
+from .currencies import Currencies
+from .currency_loader import CurrencyLoader
+from .currency_builder_cli import CurrencyBuilderCLI
