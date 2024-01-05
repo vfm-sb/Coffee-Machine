244cbea9f886c441e7584ca5e299d61ed73c43db
VFM | SB <vfm_sb@proton.me>
2024-01-05 22:46:47 +0200

    File Utils >> New: file_exists() & list_files() Functions
    + Consequent Changes on utils/__init__.py
    
    modified:   coffee_machine/utils/__init__.py
    modified:   coffee_machine/utils/file_utils.py

--- coffee_machine/utils/file_utils.py
+++ coffee_machine/utils/file_utils.py
@@ -16,3 +16,28 @@ def get_folder_path(path: str) -> str:
 
 def get_file_path(filename: str, path: str) -> str:
     return get_folder_path(path) + filename
+
+
+def file_exists(filename: str, path: str) -> bool:
+    file_path = get_file_path(filename, path)
+    return os.path.exists(file_path)
+
+
+def list_files(path: str, extension: str | None = None, excluded: list | None = None) -> list:
+    if extension is None:
+        extension = ""
+    if excluded is None:
+        excluded = []
+    all_files = os.listdir(get_folder_path(path))
+    filtered_files = []
+    for file in all_files:
+        if file in excluded:
+            continue
+        if file.endswith(extension):
+            filtered_files.append(file)
+    return filtered_files
+
+
+# Testing
+if __name__ == "__main__":
+    pass
