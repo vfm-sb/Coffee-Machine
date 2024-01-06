0f1dc0993d48df8c93732b2c0a84e97a6bbeb297
VFM | SB <vfm_sb@proton.me>
2024-01-06 23:52:45 +0200

    Major Re-Work: CurrencyHelper Class & Consequent Changes
    - New: CurrencyHelper Class
    - Moved: from Currency to CurrencyHelper >> All Assert Methods
    - Moved: from Currency to CurrencyHelper >> All Validators
    - Consequent Changes Applied to Currency & CurrencyHelper
    - Removed: Currencies
    - Moved: from Currencies to CurrencyHelper >> Core Tasks
    - Updated: electrum/__init__.py
    
    modified:   coffee_machine/electrum/__init__.py
    modified:   coffee_machine/electrum/currency.py
    new file:   coffee_machine/electrum/currency_helper.py
    modified:   coffee_machine/electrum/currency_loader.py
    deleted:    coffee_machine/electrum/currencies.py

--- coffee_machine/electrum/currency_loader.py
+++ coffee_machine/electrum/currency_loader.py
@@ -1,34 +1,19 @@
-# Built-in Modules
-import json
-
 # Local Modules
-from coffee_machine.electrum import Currencies
-
-# Utilities
-from coffee_machine.utils import get_file_path
+from coffee_machine.electrum import CurrencyHelper
 
 
-class CurrencyLoader:
+class CurrencyLoader(CurrencyHelper):
 
-    def __init__(self, code: str | int) -> None:
-        currencies = Currencies()
-        self.currency_id = currencies.get_currency_id(code)
-        self.currency_data = self.retrieve_currency_data()
-
-    def retrieve_currency_data(self) -> dict:
-        filename = f"{self.currency_id}.{Currencies.extension}"
-        filepath = get_file_path(filename, path=Currencies.path)
-        with open(filepath, "r", encoding="UTF-8") as currency_file:
-            return json.load(currency_file)
-
-    @property
-    def data(self):
-        return self.currency_data
+    def __init__(self, code: str) -> None:
+        super().__init__()
+        CurrencyHelper.assert_currency(code)
+        self.currency_data = self.retrieve_currency_data(code)
 
     @classmethod
     def load(cls, code: str | int) -> dict:
         currency = cls(code)
-        return currency.data
+        return currency.currency_data
+
 
 # Testing
 if __name__ == "__main__":
